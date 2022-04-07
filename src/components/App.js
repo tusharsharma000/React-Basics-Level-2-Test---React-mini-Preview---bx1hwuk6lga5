de4980b5c58257a6874d175edd34c89be4831abd
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-04-07 20:19:23 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,37 @@
-import React from 'react'
-import '../styles/App.css';
-import { Preview } from './Preview';
+import React, { useState } from "react";
+import "../styles/App.css";
+import { Preview } from "./Preview";
 const App = () => {
+  const [state1, setState1] = useState("hello world");
+  const [state2, setState2] = useState(10);
+  const [state3, setState3] = useState(5);
 
   return (
     <div id="main">
       <div>
-        {/* add input elememnts inside here */}
+        <input
+          type="text"
+          id="contentInput"
+          value={state1}
+          onChange={(e) => setState1(e.target.value)}
+        />
+        <input
+          type="number"
+          id="fontSizeInput"
+          value={state2}
+          onChange={(e) => setState2(e.target.value)}
+        />
+        <input
+          type="number"
+          id="paddingInput"
+          value={state3}
+          onChange={(e) => setState3(e.target.value)}
+        />
       </div>
- 
- {/* render Preview component here */}
- 
-    </div>
-  )
-}
 
+      <Preview padding={state3} fontSize={state2} content={state1} />
+    </div>
+  );
+};
 
 export default App;
